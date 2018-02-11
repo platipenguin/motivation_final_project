65e55ec3d6746244c846657ba890f00d48040206
platipenguin <elliotm555@gmail.com>
2018-02-11 03:28:20 -0500

    Finished implementing clinician

--- clinician.js
+++ clinician.js
@@ -40,6 +40,11 @@ function testOptionClicked(clickedID) {
 			document.getElementById(clickedID).disabled = true;
 	}
 	
+	if (runningTests.includes(id)) // Let user cancel tests that are in progress
+	{
+		document.getElementById("runButton").disabled = false;
+		document.getElementById("runButton").innerHTML = "Cancel Test";
+	}
 	if (!completedTests.includes(testData.data[id].name)) // Show info about test and prepare run button
 	{
 		document.getElementById("testInfo").className = "";
@@ -60,8 +65,38 @@ function testOptionClicked(clickedID) {
 
 // Called when the user clicks the 'Run Test' button
 // Moves the current test into the 'Running Tests' category, starts to run the test
+// Also used to cancel a test that is being run
 function runTest() {
 	var t = document.getElementById(`${testOnDeck}`);
+	
+	// Handle cancelling the test
+	if (document.getElementById("runButton").innerHTML == "Cancel Test")
+	{
+		if (runningTests.indexOf(testOnDeck) != -1)
+		{
+			if (runningTests.length > 1)
+			{
+				var i = runningTests.indexOf(testOnDeck);
+				runningTests = (i == 0) ? [runningTests[1]] : [runningTests[0]];
+				runningTestTimes = (i == 0) ? [runningTestTimes[1]] : [runningTestTimes[0]];
+			}
+			else
+			{
+				runningTests.pop();
+				runningTestTimes.pop();
+			}
+			var cancel = document.getElementById(`${testOnDeck}`);
+			cancel.disabled = false;
+			cancel.innerHTML = testData.data[testOnDeck].name;
+			document.getElementById("progressTests").removeChild(cancel);
+			document.getElementById("tests").appendChild(cancel);
+		}
+		testOnDeck = null;
+		document.getElementById("runButton").innerHTML = "Run Test";
+		document.getElementById("runButton").disabled = true;
+		return;
+	}
+	
 	if (testData.data[testOnDeck].time != 0)
 	{
 		if (runningTests.length < 2)
@@ -126,6 +161,107 @@ function tickClock() {
 				tickFunction = setTimeout(tickClock, 1100);
 		}
 	}
+}
+
+// Called when the user clicks on the 'Write Report' button
+// Hides the bench, shows the email composition screen, then populates the selection menus with completed tests
+function writeReport() {
+	if (document.getElementById("reportButton").innerHTML == "Write Report")
+	{
+		document.getElementById("reportButton").innerHTML = "Back";
+		document.getElementById("bench").className = "dashboard hidden";
+		document.getElementById("progressTests").className = "hidden";
+		document.getElementById("completeTests").className = "vertical hidden";
+		document.getElementById("tests").className = "dashboard vertical hidden";
+		document.getElementById("emailBox").className = "dashboard";
+		
+		var pathSelect = document.getElementById("pathogenSelect");
+		var idSelect = document.getElementById("identificationSelect");
+		var fingerSelect = document.getElementById("fingerprintSelect");
+		if (pathSelect.length == 0)
+		{
+			var paths = ["Norovirus", "Camphylobacter jejuni", "Clostridium perfringens", "Salmonella enterica", "Staphylococcus aureus", "Entamoeba histolytica", "Giardia lamblia"];
+			for (var i = 0; i < paths.length; i++)
+			{
+				var toAdd = document.createElement("option");
+				toAdd.text = paths[i];
+				pathSelect.appendChild(toAdd);
+			}
+		}
+		for (var i = idSelect.length; i < completedTests.length; i++)
+		{
+			var toAdd1 = document.createElement("option");
+			var toAdd2 = document.createElement("option");
+			toAdd1.text = completedTests[i];
+			toAdd2.text = completedTests[i];
+			idSelect.appendChild(toAdd1);
+			fingerSelect.appendChild(toAdd2);
+		}
+	}
+	else
+	{
+		document.getElementById("reportButton").innerHTML = "Write Report";
+		document.getElementById("bench").className = "dashboard";
+		document.getElementById("progressTests").className = "";
+		document.getElementById("completeTests").className = "vertical";
+		document.getElementById("tests").className = "dashboard vertical";
+		document.getElementById("emailBox").className = "dashboard hidden";
+		document.getElementById("directorMail").className = "dashboard hidden";
+	}
+}
+
+// Called when the user clicks on the 'Send Email' button
+// Moves to the 'You've got mail!' transition screen
+function sendMail() {
+	document.getElementById("emailBox").className = "dashboard hidden";
+	document.getElementById("gotMailBox").className = "dashboard";
+	document.getElementById("reportButton").disabled = true;
+	setTimeout(startMailAnimation, 100);
+}
+
+function startMailAnimation() {
+	document.getElementById("mailAlert").style = "color: black; transition: color 4s;";
+	document.getElementById("mailImg").style = "opacity: 1.0; transition: opacity 4s;";
+}
+
+// Called when the user clicks on the 'Open Email' button
+// Moves to the email from the director screen, and based on the items they chose in their report,
+// determines if the director will be happy with their report or want them to try again
+function openEmail() {
+	document.getElementById("gotMailBox").className = "dashboard hidden";
+	document.getElementById("directorMail").className = "dashboard";
+	document.getElementById("mailAlert").style = "";
+	document.getElementById("mailImg").style = "";
+	
+	var emailBody = "Agent *,<br><br>";
+	if (document.getElementById("pathogenSelect").value != "Salmonella enterica")
+	{
+		emailBody += "I looked over the results you sent me and I'm not entirely convinced that " + document.getElementById("pathogenSelect").value;
+		emailBody += " is behind the current outbreak. Please review your data, then get back to me.<br><br>";
+		document.getElementById("reportButton").disabled = false;
+	}
+	else if (document.getElementById("identificationSelect").value != "Bismuth Sulfite Agar Culture")
+	{
+		emailBody += "I think you might be on to something that Salmonella is behind the most recent outbreak, though the data you sent me does not prove that. ";
+		emailBody += "Once you have a test showing that Salmonella is our pathogen, let me know.<br><br>";
+		document.getElementById("reportButton").disabled = false;
+	}
+	else if (document.getElementById("fingerprintSelect").value != "S. enterica DNA Fingerprint")
+	{
+		emailBody += "Good work identifying Salmonella as the pathogen behind this outbreak. I'm convinced it's our culprit. ";
+		emailBody += "The second set of test data you sent me, however, does not prove that all the patients were infected from the same source. We need that information to deal with this outbreak properly.<br><br>";
+		emailBody += "Perform some more tests, and once you have data showing whether or not this is an actual epidemic, send it my way.<br><br>";
+		document.getElementById("reportButton").disabled = false;
+	}
+	else
+	{
+		emailBody += "Great work. Your results show that Salmonella is the pathogen behind this outbreak, and the DNA fingerprint data you sent also proves that all the patients contracted it from the same source.<br><br>";
+		emailBody += "The clinical side of this investigation is done. Chances are, though, Agent ** is still interviewing patients. Go see if you can help them out. The information you've gathered should help dial in the questions.<br><br>";
+		emailBody += "Again, great work, agent. Just remember, this isn't over yet!<br><br>";
+	}
+	
+	emailBody += "Best,<br><br>Viola Greene<br><br>Director, Office of Public Health Preparedness and Response<br>Centers for Disease Control";
+	document.getElementById("directorMailText").innerHTML = emailBody;
 }
 
 // Called when the user clicks the 'Show Notepad' button
