e68ecaf4befb21876f37724c7d42a25cfae1566d
platipenguin <elliotm555@gmail.com>
2018-02-14 13:16:02 -0500

    Add intro email and layout changes
    Now get email from director after entering names, then choose role. Reduced height on some of the dashboards to work better on smaller screens.

--- selectionPage.js
+++ selectionPage.js
@@ -1,3 +1,25 @@
+// Called when the user clicks on the 'Start' button
+// Moves to the 'You've got mail!' transition screen
+function start() {
+	document.getElementById("titleBox").className = "dashboard hidden";
+	document.getElementById("gotMailBox").className = "dashboard";
+	setTimeout(startMailAnimation, 100);
+}
+
+function startMailAnimation() {
+	document.getElementById("mailAlert").style = "color: black; transition: color 4s;";
+	document.getElementById("mailImg").style = "opacity: 1.0; transition: opacity 4s;";
+}
+
+// Called when the user clicks the 'Open Email' button
+// Moves to the next screen
+function openEmail() {
+	document.getElementById("gotMailBox").className = "hidden";
+	document.getElementById("mailBox").className = "dashboard";
+}
+
+// Called when the user clicks on one of the role selection buttons
+// Redirects the web browser to the file for that role
 function goToActivity(page) {
     localStorage.userName = document.getElementById("userName");
     localStorage.partnerName = document.getElementById("partnerName");
